d5a74ff1e409af976413d853f3eadca5fbd8bce0
Ian <ianbarbera@gmail.com>
2022-06-24 01:44:03 -0300

    creatd custom ui components : card and button

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- v-bind class https://vuejs.org/guide/essentials/class-and-style.html --->
+  <button class="button" :class="[color, { 'full-width': fullWidth }]">
+    {{ text }}
+  </button>
+</template>
+  
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+    fullWidth: Boolean,
+  },
+};
+</script>
+
+<style lang="scss">
+.button {
+  background: #0099ff;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-weight: bold;
+  color: white;
+  margin: 20px;
+
+  &:hover {
+    filter: brightness(80%);
+  }
+}
+
+.green {
+  background: green;
+
+  &:hover {
+    filter: brightness(80%);
+  }
+}
+
+.red {
+  background: red;
+
+  &:hover {
+    filter: brightness(80%);
+  }
+}
+
+.full-width {
+  width: 100%;
+}
+</style>
---
+++ src/components/ui/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR31da5RtxSPvf2xLuMRUHuyCXV7EhCWru6x_vg6vlIChTfMiYpVHFHG8r_p6sLdCrW8sY&usqp=CAU"
+      alt="Bigote"
+    />
+    <h1>{{ title }}</h1>
+    <p>
+      {{ description }}
+    </p>
+    <Button text="SAPE" color="red" fullWidth />
+  </div>
+</template>
+
+<script>
+import Button from "@/components/ui/Button.vue";
+
+export default {
+  name: "Card",
+  components: { Button },
+  props: {
+    title: String,
+    description: String,
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  font-family: Arial;
+  border: 1px solid #c0c0c0;
+  width: 300px;
+  border-radius: 10px;
+}
+
+h1 {
+  margin: 40px 0 0;
+  color: black;
+  margin: 20px;
+}
+
+p {
+  color: #696969;
+  margin: 20px;
+}
+
+img {
+  width: 100%;
+}
+</style>
